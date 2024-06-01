92632ddfcfdbf4bdcee28d85c2515e98f5b69a9f
lololfo <rodoemma12@gmail.com>
2024-06-01 16:01:50 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "CHOOBY";
+const meta = {
+    title: "John Doe",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Soy Rodolfo, alias el chooby",
+    animated: {
+        first: "Me encanta programar",
+        second: "Programo exelentes sitios web",
+        third: "Y desarrollo aplicaciones moviles",
+    },
+    description: "Soy una persona apasionada, que aprecia la opinion del cliente para entregar un trabajo excepcional",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "¿Quién soy yo?",
+    aboutme: "¡Hola! Soy Rodolfo Nuñez, mejor conocido como Chooby, un apasionado desarrollador web con una habilidad especial para crear aplicaciones web dinámicas y responsivas. Con una formación en la Universidad Tecnológica del Sur de Sonora (UTS), me especializo tanto en el desarrollo front-end como en el back-end, esforzándome por ofrecer experiencias de usuario impecables a través de un código limpio y eficiente.",
+};
+const worktimeline = [{
+    jobtitle: "Desarrollador aplicaiones moviles",
+    where: "UTS",
+    date: "2022",
+},
+{
+    jobtitle: "Desarrollador web (front)",
+    where: "UTS",
+    date: "2023",
+},
+{
+    jobtitle: "Tester",
+    where: "UTS",
+    date: "2023",
+},
+];
+
+const habilidades = [{
+    name: "Angular",
+    value: 90,
+},
+{
+    name: "ReactJs",
+    value: 85,
+},
+{
+    name: "SqlServer",
+    value: 70,
+},
+{
+    name: "PostgreSQL",
+    value: 40,
+},
+{
+    name: "Java",
+    value: 70,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Mobile Apps",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rodoemma12@gmail.com",
+    YOUR_FONE: "(644)193-0503",
+    description: "Soy una persona apasionada, que aprecia la opinion del cliente para entregar un trabajo excepcional ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    habilidades,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
